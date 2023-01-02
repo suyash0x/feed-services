a659c4aed3a1c3e90caee725f65ac72df7fb7b8c
suyashio <suyash.io.dev@gmail.com>
2023-01-02 17:40:20 +0530

    Updated description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const express = require("express");
 const app = express();
-app.listen("5000", () => console.log("Field services are up and running on port number 5000..."));
+app.listen("5000", () => console.log("Feed services are up and running on port number 5000..."));
