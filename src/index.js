a6447e73b94aa2700e624f0ca2cc1b2238d51649
suyashio <suyash.io.dev@gmail.com>
2023-01-02 17:31:45 +0530

    shifted to javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { Express } from "express";
 const express = require("express");
-const app: Express = express();
+const app = express();
 app.listen("5000", () => console.log("Field services are up and running on port number 5000..."));
